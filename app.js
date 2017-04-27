6981cee0061c2a2286dbb1c8d97d333c4cde8a26
Brian Sweeney <bsweeney930@gmail.com>
2017-04-27 16:40:59 -0400

    beginnings of saveClick function

--- app.js
+++ app.js
@@ -5,40 +5,41 @@ var angularApp = angular.module('angularApp', []);
 angularApp.controller('mainController', ['$scope', '$filter', '$log', function ($scope, $filter, $log) {
     
     
-    $scope.handle = ''
+    $scope.start = '';
+    $scope.stop = '';
     
-    $scope.lowercaseHandle = function(){
-        return $filter('lowercase')($scope.handle)
+    $scope.saveClip = function(){
+        $scope.clips.push({
+            id: clips.length,
+            timeInfo: {
+                start: $scope.start,
+                stop: $scope.stop
+            }
+        })
     }
     
     $scope.characters = 5;
     
     $scope.clips = [
         {id: 1,
-             timeInfo:{
-                start:1,
-                stop: 1
+         timeInfo:{
+            start:1,
+            stop: 1
             }
         },
         {id: 2,
-            timeInfo:{
-                start:1,
-                stop: 1
+        timeInfo:{
+            start:1,
+            stop: 1
             }
         },
         {id: 3,
-            timeInfo:{
-                start:1,
-                stop: 1
+        timeInfo:{
+            start:1,
+            stop: 1
             }
         }
     ]
-    for(var i = 0; i < $scope.clips.length; i++){
-        $log.log($scope.clips[i].timeInfo.start)
-    }
-        $scope.alertClick = function(){
-            alert('sup')
-        }
 }]);
 
 
