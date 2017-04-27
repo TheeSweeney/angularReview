e201d4b67989526475c1630014cdbd1d876e4388
Brian Sweeney <bsweeney930@gmail.com>
2017-04-27 14:29:01 -0400

    basic watcher

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+angularApp.controller('mainController', ['$scope', '$filter', '$log', function ($scope, $filter, $log) {
     
     
     $scope.handle = ''
@@ -11,6 +11,11 @@ angularApp.controller('mainController', ['$scope', '$filter', function ($scope, 
         return $filter('lowercase')($scope.handle)
     }
     
+    $scope.$watch('handle', function(newValue, oldValue) {
+        $log.info('Changed')
+        $log.log('Old: ' + oldValue);
+        $log.log('New: ' + newValue);
+    })
 }]);
 
 
